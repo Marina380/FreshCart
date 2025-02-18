fdb265538c8753e2edc534d5f38649d43a7e4005
marina <marinatharwat49@gmail.com>
2025-02-19 00:38:50 +0200

    'Done'

--- src/Component/AllOrder/AllOrder.jsx
+++ src/Component/AllOrder/AllOrder.jsx
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { OrderContext } from "../../Context/OrderContext";
 import logo from "./../../assets/finalProject assets/images/order.jpg";
 
 export default function AllOrder() {
   let { gettLoggedCart, addOrder } = useContext(OrderContext);
   console.log(addOrder?.data);
+
+  useEffect(()=> gettLoggedCart ,
+[])
 
   return (
     <>
--- src/Context/OrderContext.jsx
+++ src/Context/OrderContext.jsx
@@ -1,38 +1,57 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-import { createContext, useEffect, useState } from "react";
-import toast from 'react-hot-toast';
+import { createContext , useState } from "react";
 
 
 export let OrderContext = createContext();
 
 export default function OrderContextProvider(props) {  
-// const [addOrder, setAddOrder] = useState(null)
-// let decoded = jwtDecode(localStorage.getItem("userToken"));
+ let  [addOrder, setAddOrder] = useState(null)
+
+ let [decoded, setdecoded] = useState(null)
  
-//   async  function gettLoggedCart() {
+
+ async function gettLoggedCart() {
+  
+
+
+  if (localStorage.getItem("userToken")){
+
+try {
+  setdecoded(jwtDecode(localStorage.getItem("userToken")));
+  let {data}=  await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${ decoded}`)
+console.log(data);
+ setAddOrder(data)
+  
+} catch (error) {
+  console.log(error);
+  
+}
+ 
 
 
 
-//     try {
-//         let {data}=  await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${decoded}`)
-//         console.log(data);
-//          setAddOrder(data)
+  }
 
-//     } catch (error) {
-//         console.log(error);
-        
-//     }
-//   }
+ }
+  
+
+  
+
+
+
+
+
+
 
  
-  //  useEffect(()=>{
-  //    gettLoggedCart()
-  //  },[])
+
+ 
+
 
   return (
-    <OrderContext.Provider value={{      }}>
+    <OrderContext.Provider value={{  addOrder ,   gettLoggedCart }}>
       {props.children} 
     </OrderContext.Provider>
   );
