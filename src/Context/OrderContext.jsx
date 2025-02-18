4ed577e8e14355ee04f8b3aeade897431fcf73b5
marina <marinatharwat49@gmail.com>
2025-02-18 23:12:54 +0200

    'Done'

--- src/Context/OrderContext.jsx
+++ src/Context/OrderContext.jsx
@@ -8,31 +8,31 @@ import toast from 'react-hot-toast';
 export let OrderContext = createContext();
 
 export default function OrderContextProvider(props) {  
-const [addOrder, setAddOrder] = useState(null)
-let decoded = jwtDecode(localStorage.getItem("userToken"));
+// const [addOrder, setAddOrder] = useState(null)
+// let decoded = jwtDecode(localStorage.getItem("userToken"));
  
-  async  function gettLoggedCart() {
+//   async  function gettLoggedCart() {
 
 
 
-    try {
-        let {data}=  await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${decoded}`)
-        console.log(data);
-         setAddOrder(data)
+//     try {
+//         let {data}=  await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${decoded}`)
+//         console.log(data);
+//          setAddOrder(data)
 
-    } catch (error) {
-        console.log(error);
+//     } catch (error) {
+//         console.log(error);
         
-    }
-  }
+//     }
+//   }
 
  
-   useEffect(()=>{
-     gettLoggedCart()
-   },[])
+  //  useEffect(()=>{
+  //    gettLoggedCart()
+  //  },[])
 
   return (
-    <OrderContext.Provider value={{    addOrder , gettLoggedCart  }}>
+    <OrderContext.Provider value={{      }}>
       {props.children} 
     </OrderContext.Provider>
   );
